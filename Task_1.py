01ee7078ae274cf22ded7ef173c913caf540f1f0
KarenRouge <karenrouge1991@gmail.com>
2021-11-11 19:50:48 -0500

    Task_1 complete

---
+++ Task_1.py
@@ -0,0 +1,9 @@
+number =input('Введите число')
+print(number)
+animal = input("Назовите Ваше любимое животное")
+print(animal)
+a = int(input('Введите первое число'))
+b = int(input('Введите второе число'))
+sum = a + b
+print(sum)
+
